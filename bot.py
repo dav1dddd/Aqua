747f9c9492fc6d4ed612ad301088459e72357c39
dps910 <dps141618@gmail.com>
2020-04-26 23:06:24 +0100

    command handler
    so I don't have to keep doing [cogs.x, cogs.y], because it's retarded

--- bot.py
+++ bot.py
@@ -10,14 +10,22 @@ sentry_sdk.init(os.getenv("SENTRY_DSN"))
 bot = commands.Bot(command_prefix="!", activity=discord.Game(name="made by dps#0001"))
 
 # Load cogs
-cogs = ["cogs.ping", "cogs.yiff", "cogs.neko", "cogs.echo", "cogs.kick"]
-for c in cogs: 
-    bot.load_extension(c)
+for c in os.listdir(path=f"./cogs"):
+    # Check if files end with .py
+    if c.endswith(".py"):
+        # Split .py, then remove .py and get only the 'root' and not the 'ext'
+        split = os.path.splitext(c)[0]
+        # Append "cogs." to filename, and load the cogs :D
+        bot.load_extension(f"cogs.{split}")
 
 # Load events
-events = ["events.ready", "events.on_message"]
-for e in events:
-    bot.load_extension(e)
+for e in os.listdir(path=f"./events"):
+    # Check if files end with .py
+    if e.endswith(".py"):
+        # Split .py, then remove .py and get only the 'root' and not the 'ext'
+        split = os.path.splitext(e)[0]
+        # Append "events." to filename, and load the events :D
+        bot.load_extension(f"events.{split}")
 
 # Connect to the Discord API
 if __name__ == "__main__":
