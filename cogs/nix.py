d65abbe47a6934c3fa03cddb11b99d0f439a2eb4
david <davidpetersmith@protonmail.com>
2020-08-29 21:16:08 +0000

    I don't know what's wrong with the md, everything else is good though

--- cogs/nix.py
+++ cogs/nix.py
@@ -1,6 +1,9 @@
 from discord import Embed
 from discord.ext import commands
 from aiohttp import ClientSession
+from json import dump
+
+status = []
 
 class Nix(commands.Cog):
     def __init__(self, bot):
@@ -9,22 +12,31 @@ class Nix(commands.Cog):
     @commands.command(
         name="nix",
         description="nix packages for Debian/Arch/RHEL based systems and macOS",
-        aliases=["pkgs"],
+        aliases=["pkgs"]
     )
-     
-    async def fetch(client): 
-        debianstable = "https://packages.debian.org/stable/allpackages"
-        async with client.get(debianstable) as response:
-            # Test if response is equal to 200.
-            # Otherwise, AssertionError is raised.
-            assert response.status == 200
-            print(status)
-            # Show the HTML of the website.
-            return await response.text()
 
     async def nix(self, ctx):
-        async with ClientSession() as client:
-            print(await fetch(client))
+        # Get the NixOS github JSON
+        nixos_repo = "https://api.githu.com/users/NixOS"
+        async with ClientSession() as session:
+            async with session.get(nixos_repo) as response:
+                if (response.status == 200):
+                    json = await response.json()
+                    
+                    # Write this data to a file so it can be easily parsed and accessed.
+                    # The with statement will automatically close the file, so we don't have to do anything.
+                    with open("nixos.json", "w", encoding="UTF-8") as f:
+                        dump(json, f, indent=4)
+
+    # Use decorator to handle discordpy exception "CommandInvokeError"
+    @nix.error
+    async def nix_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError): # isinstance basically means "the same as"
+            # Response should be 200, but if it isn't, let the user know.
+            await ctx.send(embed=Embed(
+                title="⚠️ Couldn't access the GitHub API. You can check the status [here](https://www.githubstatus.com).",
+                colour=0xec7063
+            ))
 
 def setup(bot):
     bot.add_cog(Nix(bot))
