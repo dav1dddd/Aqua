d3bdd0c613509191831f9642080d727e17a62a31
david <davidpetersmith@protonmail.com>
2020-08-30 20:28:51 +0000

    do "git search user" instead of just "git user"

--- cogs/github.py
+++ cogs/github.py
@@ -13,46 +13,53 @@ class GitHub(commands.Cog):
         aliases=["git"]
     )
 
-    async def git(self, ctx, user=None):
-        if user == None:
+    async def git(self, ctx, *args: str):
+        if not args:
+            await ctx.send(
+                "You need to include some arguments for this to do anything. Here is what's possible as of now. `user`"
+            )
             return
+        elif args[0] != "user":
+            await ctx.send("No, it is `git user`")
+            return
+        else:
         # Search for a GitHub user.
-        async with ClientSession(headers={"Accept": "application/vnd.github.v3+json"}) as session:
-            users = f"https://api.github.com/users/{user}"
-            async with session.get(users) as response:
-                json = await response.json()
-                name = json["login"]
-                avatar_url = json["avatar_url"]
-                profile = json["html_url"]
-                repos = json["public_repos"]
-                gists = json["public_gists"]
-                followers = json["followers"]
-                following = json["following"]
-                website = json["blog"]
-                twitter = json["twitter_username"]
-                
-                if website == "":
-                    website = None
+            async with ClientSession(headers={"Accept": "application/vnd.github.v3+json"}) as session:
+                users = f"https://api.github.com/users/{args[1]}"
+                async with session.get(users) as response:
+                    json = await response.json()
+                    name = json["login"]
+                    avatar_url = json["avatar_url"]
+                    profile = json["html_url"]
+                    repos = json["public_repos"]
+                    gists = json["public_gists"]
+                    followers = json["followers"]
+                    following = json["following"]
+                    website = json["blog"]
+                    twitter = json["twitter_username"]
+                    
+                    if website == "":
+                        website = None
 
-                await ctx.send(
-                        embed=Embed(
-                            title="GitHub user",
-                            colour=0x7DCEA0,
+                    await ctx.send(
+                            embed=Embed(
+                                title="GitHub user",
+                                colour=0x7DCEA0,
+                            )
+                            .add_field(name="Profile", value=f"[{name}]({profile})", inline=False)
+                            .add_field(name="Repositories", value=repos, inline=True)
+                            .add_field(name="Gists", value=gists, inline=True)
+
+                            # Here I add an empty field as I want 2 rows with 2 columns
+                            .add_field(name="\u200b", value="\u200b", inline=True)
+
+                            .add_field(name="Following", value=following, inline=True)
+                            .add_field(name="Followers", value=followers, inline=True)
+                            .add_field(name="Website", value=website, inline=False)
+                            .add_field(name="Twitter", value=twitter, inline=True)
+                            .set_thumbnail(url=avatar_url)
+                            .set_footer(text=f"Created at | {datetime.now().strftime('%d.%m.%Y')}")
                         )
-                        .add_field(name="Profile", value=f"[{name}]({profile})", inline=False)
-                        .add_field(name="Repositories", value=repos, inline=True)
-                        .add_field(name="Gists", value=gists, inline=True)
-
-                        # Here I add an empty field as I want 2 rows with 2 columns
-                        .add_field(name="\u200b", value="\u200b", inline=True)
-
-                        .add_field(name="Following", value=following, inline=True)
-                        .add_field(name="Followers", value=followers, inline=True)
-                        .add_field(name="Website", value=website, inline=False)
-                        .add_field(name="Twitter", value=twitter, inline=True)
-                        .set_thumbnail(url=avatar_url)
-                        .set_footer(text=f"Created at | {datetime.now().strftime('%d.%m.%Y')}")
-                    )
 
     @git.error
     async def git_error(self, ctx, error):
